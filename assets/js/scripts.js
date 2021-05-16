020d1653c2a0e9c654a076b3e1abe53a12969151
philip howley <howley.phil@gmail.com>
2021-05-16 12:31:12 -0400

    Add home page

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+    // console.log('hello javascript is working!');
+    // var myHeaderEl = document.querySelector('.header');
+    // console.log(myHeaderEl)
+    // var myName = 'Phil'; // string 
+    // var isAFunnyGuy = true;
+    // var age = 21;
+    // console.log(myName);
+    // var myH1El = document.createElement('h1');
+    // myH1El.textContent = 'Do you see how cool this is?';
+    // myHeaderEl.appendChild(myH1El);
+
+    function greet(name, age = 33) {
+      // console.log('hello '+ name);
+      // console.log('i am ' + age + ' years old');
+    }
+
+    greet('Phil');
+    greet('Mark');
+    greet('Victor');
